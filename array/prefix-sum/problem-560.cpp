b142114c309863474bbc5018f9c969b2f1a6395d
devi5040 <dpraidola@gmail.com>
2025-11-26 12:02:16 +0530

    feat: add solution for "Subarray Sum Equals K" problem using prefix sum and hashmap

--- array/prefix-sum/problem-560.cpp
+++ array/prefix-sum/problem-560.cpp
@@ -1,1 +1,18 @@
 // Subarray sum equals k
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int count = 0, runningPrefix = 0;
+        unordered_map<int, int> freq;
+        freq[0] = 1;
+        for(int i=0;i<nums.size();i++)
+        {
+            runningPrefix += nums[i];
+            if(freq.count(runningPrefix - k)>0)
+                count += freq[runningPrefix - k];
+            freq[runningPrefix]++;
+        }
+        return count;
+    }
+};
