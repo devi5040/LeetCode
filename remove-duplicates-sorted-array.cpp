1155a7e0f45cd192f4bcfa9c59339c16b76e24a4
devi5040 <dpraidola@gmail.com>
2026-04-25 16:26:16 +0530

    feat: Implement removeDuplicates function to eliminate duplicates from a sorted array

---
+++ remove-duplicates-sorted-array.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    int removeDuplicates(vector<int> &nums)
+    {
+        int current = nums[0], index = 1;
+
+        for (int i = 1; i < nums.size(); i++)
+        {
+            if (nums[i] > current)
+            {
+                current = nums[i];
+                swap(nums[i], nums[index]);
+                index++;
+            }
+        }
+
+        return index;
+    }
+};
