9670e21cc88d17d5dfcdf94947b032f8999fb0a6
Deviprasad Rai P <91744010+devi5040@users.noreply.github.com>
2026-06-01 11:09:10 +0530

    Add bucket sort and heap techniques for top K frequent elements

--- top-k-frequent-elements.cpp
+++ top-k-frequent-elements.cpp
@@ -19,3 +19,48 @@ public:
         return res;
     }
 };
+
+
+// Bucket sort (Optimal)
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        int max_val, max_index = 0;
+        unordered_map<int, int> freq;
+        vector<int> res;
+        vector<vector<int>> bucket(nums.size()+1);
+        for(int num: nums)
+            freq[num]++;
+        for(auto &it:freq)
+            bucket[it.second].push_back(it.first);
+        for(int i=nums.size(); i>=0 && res.size()<k; i--){
+            for(int num: bucket[i]){
+                res.push_back(num);
+                if(res.size()==k)
+                    return res;
+            }
+        }
+        return res;
+    }
+};
+
+// Heap technique (Priority queue)
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        int max_val, max_index = 0;
+        unordered_map<int, int> freq;
+        vector<int> res;
+        priority_queue<pair<int, int>> pq;
+        for(int num: nums)
+            freq[num]++;
+        for(auto &it:freq)
+            pq.push({it.second, it.first});
+        while(k--)
+        {
+            res.push_back(pq.top().second);
+            pq.pop();
+        }
+        return res;
+    }
+};
