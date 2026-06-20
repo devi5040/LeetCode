be6647d7928928daec39dd3cd80c362b94de0fd4
devi5040 <dpraidola@gmail.com>
2026-06-20 12:16:20 +0530

    feat: implement daily temperatures solution using a monotonic stack to find the next warmer day efficiently for each temperature.

---
+++ daily-temperatures.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    vector<int> dailyTemperatures(vector<int> &temperatures)
+    {
+        stack<int> st;
+        int n = temperatures.size();
+        vector<int> result(n, 0);
+        for (int i = 0; i < n; i++)
+        {
+            while (!st.empty() && temperatures[i] > temperatures[st.top()])
+            {
+                int m = st.top();
+                st.pop();
+                result[m] = i - m;
+            }
+            st.push(i);
+        }
+        return result;
+    }
+};
