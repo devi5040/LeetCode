85234e758f975e7c7c5c18a6a329da11fbde1dad
devi5040 <dpraidola@gmail.com>
2026-06-21 10:41:12 +0530

    feat: Implement car fleet calculation logic. Adds a new file 'car-fleet.cpp' containing a Solution class with the carFleet method, which calculates the number of car fleets based on target, positions, and speeds.

---
+++ car-fleet.cpp
@@ -0,0 +1,32 @@
+class Solution
+{
+public:
+    int carFleet(int target, vector<int> &position, vector<int> &speed)
+    {
+        vector<pair<int, int>> cars;
+
+        for (int i = 0; i < position.size(); i++)
+        {
+            cars.push_back({position[i], speed[i]});
+        }
+
+        sort(cars.begin(), cars.end());
+
+        int fleets = 0;
+        double lastTime = 0;
+
+        for (int i = cars.size() - 1; i >= 0; i--)
+        {
+            double curTime =
+                (double)(target - cars[i].first) / cars[i].second;
+
+            if (curTime > lastTime)
+            {
+                fleets++;
+                lastTime = curTime;
+            }
+        }
+
+        return fleets;
+    }
+};
