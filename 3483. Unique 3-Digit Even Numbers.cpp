ed7a7b4067a059800b907db8013af954891b32c1
devi5040 <dpraidola@gmail.com>
2026-09-11 09:26:04 +0530

    feat: Implement Unique 3-Digit Even Numbers solution. Adds `totalNumbers` function for counting unique 3-digit even numbers from given digits.

---
+++ 3483. Unique 3-Digit Even Numbers.cpp
@@ -0,0 +1,41 @@
+class Solution
+{
+public:
+    int totalNumbers(vector<int> &digits)
+    {
+        int cnt[10] = {};
+
+        for (int d : digits)
+            cnt[d]++;
+
+        int res = 0;
+
+        for (int a = 1; a <= 9; a++)
+        {
+            if (cnt[a] == 0)
+                continue;
+
+            cnt[a]--;
+
+            for (int b = 0; b <= 9; b++)
+            {
+                if (cnt[b] == 0)
+                    continue;
+
+                cnt[b]--;
+
+                for (int c = 0; c <= 8; c += 2)
+                {
+                    if (cnt[c] > 0)
+                        res++;
+                }
+
+                cnt[b]++;
+            }
+
+            cnt[a]++;
+        }
+
+        return res;
+    }
+};
