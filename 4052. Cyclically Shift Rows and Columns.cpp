3b99ef1529ec456857505ffb20b9359678fca860
devi5040 <dpraidola@gmail.com>
2026-09-14 20:14:36 +0530

    feat: Implement Rectangle Overlap solution. Adds `isRectangleOverlap` function to determine if two rectangles overlap.

---
+++ 4052. Cyclically Shift Rows and Columns.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+public:
+    vector<vector<int>> cyclicShift(int n, vector<vector<int>> &grid, vector<int> &rowShift, vector<int> &colShift)
+    {
+        vector<vector<int>> temp(n, vector<int>(n));
+
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+            {
+                int newCol = (j - rowShift[i] + n) % n;
+                temp[i][newCol] = grid[i][j];
+            }
+
+        vector<vector<int>> result(n, vector<int>(n));
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+            {
+                int newRow = (i - colShift[j] + n) % n;
+                result[newRow][j] = temp[i][j];
+            }
+
+        return result;
+    }
+};
