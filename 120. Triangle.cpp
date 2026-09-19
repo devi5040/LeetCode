6f8217040f9f4db1eeb486c514297e22d1da2247
devi5040 <dpraidola@gmail.com>
2026-09-19 16:35:56 +0530

    feat: Implement Minimum Falling Path Sum solution. Adds `minFallingPathSum` function to calculate the minimum falling path sum in a given matrix.

---
+++ 120. Triangle.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    int minimumTotal(vector<vector<int>> &triangle)
+    {
+        int m = triangle.size();
+        vector<vector<int>> dp(m, vector<int>(m));
+
+        for (int i = 0; i < m; i++)
+            dp[m - 1][i] = triangle[m - 1][i];
+
+        for (int i = m - 2; i >= 0; i--)
+            for (int j = 0; j <= i; j++)
+                dp[i][j] = triangle[i][j] + min(dp[i + 1][j], dp[i + 1][j + 1]);
+
+        return dp[0][0];
+    }
+};
---
+++ 1401. Circle and Rectangle Overlapping.cpp
@@ -0,0 +1,12 @@
+class Solution
+{
+public:
+    bool checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2)
+    {
+        int closestX = max(x1, min(xCenter, x2));
+        int closestY = max(y1, min(yCenter, y2));
+
+        int dist = sqrt(pow((closestX - xCenter), 2) + pow((closestY - yCenter), 2));
+        return dist <= radius;
+    }
+};
